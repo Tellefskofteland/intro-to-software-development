a29cc822c6a485b8fdcc6a3dde38436f6439bfac
Tellef Skofteland <155456492+Tellefskofteland@users.noreply.github.com>
2025-06-30 13:05:05 +0000

    black ckeck

--- src/geo_calculator/calculations.py
+++ src/geo_calculator/calculations.py
@@ -1,10 +1,11 @@
 def find_average(liste):
     return sum(liste) / len(liste)
 
+
 def gardners_equation(velocity: float) -> float:
     a = 0.31
-    b = 0.25 
+    b = 0.25
 
     # Calculate density using Gardner's equation
-    density = a * (velocity ** b) 
+    density = a * (velocity**b)
     return density
